00beacaf5a2dd13cbe0e2a1348dd8f1b35a3ba8e
tjdwns4537 <tjdwns4537@nate.com>
2022-10-12 18:50:15 +0900

    경사로 풀이 완료

--- baekjoon/src/경사로.java
+++ baekjoon/src/경사로.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class 경사로 {
+
+    // 백준 코드로 제출시 Main 으로 제출
+
     // 입력
     static int N, L;
     static int answer = 0;
@@ -21,98 +24,101 @@ public class 경사로 {
         }
 
         for (int i = 0; i < N; i++) {
-            if(roadCol(i) || roadRow(i)){
-                answer++;
-            }
+            answer += roadCol(i) + roadRow(i);
         }
 
         System.out.println(answer);
     }
 
-    public static boolean roadCol(int idx) {
-        int[] Buffer = new int[N];
+    public static int roadCol(int idx) {
+
+        int[] Buffer = new int[N]; // 초기값 : 0
+
+        // 만약 배열이 차이가 없는 경우에는 초기 값이 그대로 유지됨
+
         for (int i = 0; i < N; i++) {
 
             //높이 차이가 2이상 나는 경우
-            if (road[idx][i] >= road[idx][i + 1] + 2) {
-                return false;
-            }
-            if (road[idx][i] >= road[idx][i + 1] - 2) {
-                return false;
-            }
-
-            // 내리막
-            if (road[idx][i] > road[idx][i + 1]) {
-                int start = i + 1;
-                int j;
-                for (j = start; j < start + L; j++) {
-                    if(j>=N) return false; // 범위를 벗어난 경우
-                    if (road[idx][i + 1] == road[idx][j]) { // 다음 블럭의 높이가 같은 경우 버퍼에 넣어줌
-                        Buffer[j]++;
-                    }
-                    else return false;
+            try {
+                if (road[idx][i] >= road[idx][i + 1] + 2) {
+                    return 0;
                 }
-            }
-
-            // 오르막
-            if (road[idx][i] < road[idx][i + 1]) {
-                int start = i + 1;
-                int j;
-                for (j = start; j < start; j++) {
-                    if(j>=N) return false;
-                    if (road[idx][i + 1] == road[idx][j]) {
-                        Buffer[j]++;
-                    }
-                    else return false;
+                if (road[idx][i] <= road[idx][i + 1] - 2) {
+                    return 0;
                 }
+
+                if (road[idx][i] > road[idx][i + 1]) { // 내리막
+                    int start = i + 1;
+                    int j;
+                    for (j = start; j < start + L; j++) {
+                        if (j >= N) return 0; // 범위를 벗어난 경우
+                        if (road[idx][i + 1] == road[idx][j]) { // 다음 블럭의 높이가 같은 경우 버퍼에 넣어줌
+                            Buffer[j]++;
+                        } else return 0;
+                    }
+                }
+                else if (road[idx][i] < road[idx][i + 1]) { // 오르막
+                    int start = i;
+                    int j;
+                    for (j = start; j > start - L; j--) {
+                        if (j < 0) return 0; // 범위가 넘으면 종료
+                        if (road[idx][i] == road[idx][j]) {
+                            Buffer[j]++;
+                        } else return 0;
+                    }
+                }
+            } catch (Exception e) {
+
             }
         }
 
         for (int i = 0; i < N; i++) {
-            if(Buffer[i] > 1) return false;
+            if(Buffer[i] > 1) return 0;
         }
 
-        return true;
+        return 1;
     }
 
-    public static boolean roadRow(int idx) {
+    public static int roadRow(int idx) {
         int[] buffer = new int[N];
 
         for (int i = 0; i < N; i++) {
-            if (road[i][idx] >= road[i + 1][idx] + 2) {
-                return false;
-            }
-            if (road[i][idx] >= road[i + 1][idx] - 2) {
-                return false;
-            }
-
-            if (road[i][idx] > road[i + 1][idx]) {
-                int start = i + 1;
-                for (int j = start; j < start + L; j++) {
-                    if(j>=N) return false;
-                    if (road[i + 1][idx] == road[j][idx]) {
-                        buffer[j]++;
-                    }
-                    else return false;
+            try {
+                if (road[i][idx] >= road[i + 1][idx] + 2) {
+                    return 0;
                 }
-            }
-
-            if (road[i][idx] < road[i + 1][idx]) {
-                int start = i + 1;
-                for (int j = start; j < start + L; j++) {
-                    if(j>=N) return false;
-                    if (road[i + 1][idx] == road[j][idx]) {
-                        buffer[j]++;
-                    }
-                    else return false;
+                if (road[i][idx] <= road[i + 1][idx] - 2) {
+                    return 0;
                 }
+
+                if (road[i][idx] > road[i + 1][idx]) { // 오르막
+                    int start = i + 1;
+                    for (int j = start; j < start + L; j++) {
+                        if (j >= N) return 0;
+                        if (road[i + 1][idx] == road[j][idx]) {
+                            buffer[j]++;
+                        } else return 0;
+                    }
+                }
+
+                if (road[i][idx] < road[i + 1][idx]) { // 내리막
+                    int start = i;
+                    for (int j = start; j > start - L; j--) {
+                        if (j < 0) return 0;
+                        if (road[i][idx] == road[j][idx]) {
+                            buffer[j]++;
+                        } else return 0;
+                    }
+                }
+            } catch (Exception e) {
+
             }
         }
 
         for (int i = 0; i < N; i++) {
-            if(buffer[i] > 1) return false;
+            if(buffer[i] > 1) return 0;
         }
 
-        return true;
+        return 1;
     }
 }
