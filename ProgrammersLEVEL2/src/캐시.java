2969fbb95092ff17cad8a6492d85e4cf0e40573e
tjdwns4537 <tjdwns4537@nate.com>
2022-11-17 16:17:38 +0900

    1차 캐시

--- ProgrammersLEVEL2/src/캐시.java
+++ ProgrammersLEVEL2/src/캐시.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class 캐시 {
 
     /**
      - 설명 :
-     1. 테스팅 업무 > 어피치
+     1. 테스팅 업무는 어피치
      2. 제이지가 작성한 데이터베이스에서 게시물 불러오는 부분의 실행시간이 오래걸림
      3. 어피치는 제이지에게 해당 로직 개선하라고 지시
-     4. 제이지는 DB캐시를 적용하여 성능 개선을 시도하지만 효율적인 캐시 크기를 몰라 난감
+     4. 제이지는 DB 캐시를 적용하여 성능 개선을 시도하지만 효율적인 캐시 크기를 몰라 난감
 
      - 입력 형식 :
      캐시 크기 ( cacheSize ), 도시 이름 배열 ( cities )
@@ -20,15 +24,68 @@ public class 캐시 {
      cache hit일 경우 실행 시간은 1
      cache miss일 경우 실행 시간은 5
 
+     - 용어 설명 :
+     1) 캐시 히트 = 캐시 메모리에 찾는 데이터가 존재했을 때
+     2) 캐시 미스 = 캐시 메모리에 찾는 데이터가 존재하지 않았을 때
      **/
 
     public static void main(String[] args) {
-
+        int cache = 3;
+        int cache2 = 0;
+        int cache3 = 2;
+        String[] cities = {
+                "Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"
+        };
+        String[] cities2 = {
+                "Jeju", "Pangyo", "Seoul", "NewYork", "LA"
+        };
+        String[] cities3 = {
+                "Jeju", "Pangyo", "NewYork", "newyork"
+        };
+        System.out.println(solution(cache3, cities3));
     }
 
-    public int solution(int cacheSize, String[] cities) {
-        int answer = 0;
 
+    public static int solution(int cacheSize, String[] cities) {
+        for (int i = 0; i < cities.length; i++) {
+            cities[i] = cities[i].toLowerCase(Locale.ROOT);
+        }
+        int answer = LRU(cacheSize, cities);
         return answer;
+    }
+
+    public static int LRU(int s, String[] cities){
+        List<String> temp = new ArrayList<>();
+        int excuteTime = 0;
+        int cnt = 0;
+        boolean check = false;
+
+        while (cnt < cities.length) {
+            for (int i = 0; i < temp.size(); i++) {
+                if (temp.get(i).equals(cities[cnt])) {
+                    excuteTime += 1;
+                    temp.remove(temp.get(i));
+                    temp.add(0, cities[cnt]);
+                    check = true;
+                    break;
+                }
+            }
+
+            if (!check){
+                temp.add(0, cities[cnt]);
+                excuteTime += 5;
+            }
+
+            if (temp.size() > s) {
+                temp.remove(temp.size() - 1);
+            }
+
+            cnt++;
+            check = false;
+        }
+
+        excuteTime += (s - temp.size());
+
+        return excuteTime;
     }
 }
