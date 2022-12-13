9f98304fb41af59e4ea73070a626d664d250e58c
tjdwns4537 <tjdwns4537@nate.com>
2022-12-14 00:13:24 +0900

    palindrome complete

--- ProgrammersLEVEL2/src/palindrome.java
+++ ProgrammersLEVEL2/src/palindrome.java
@@ -12,49 +12,19 @@ public class palindrome {
     }
 
     public static int solution(String s) {
-        int pixIndex = 0;
-        int cnt = 0;
-        int max = 0;
-        for (int i = 1; i < s.length()-1; i++) {
-            if(s.charAt(i-1) == s.charAt(i+1)){
-                System.out.println("pick1: " + i);
-                pixIndex = i;
-                cnt = cal(s,pixIndex)*2+1;
-                max = Math.max(cnt, max);
+        for (int i = s.length(); i > 0; i--) {
+            for (int j = 0; j + i <= s.length(); j++) {
+                if(isPalindrome(s,j,j+i-1)) return i;
             }
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                System.out.println("pick2: " + i);
-                pixIndex = i;
-                cnt = cal2(s,pixIndex)*2;
-                max = Math.max(cnt, max);
-            }
-            System.out.println(max);
         }
-        return max;
+        return 0;
     }
 
-    public static int cal2(String s,int pixIndex) {
-        int cnt = 1;
-        while(true){
-            char left = s.charAt(pixIndex - cnt + 1);
-            char right = s.charAt(pixIndex + cnt);
-            System.out.println("do: " + left + " " + right);
-            if(left != right) break;
-            if(pixIndex-cnt < 0 || pixIndex+cnt+1 == s.length()) break;
-            cnt++;
+    public static boolean isPalindrome(String str, int start, int end) {
+        while (start <= end) {
+            if(str.charAt(start++) != str.charAt(end--)) return false;
         }
-        return cnt;
+        return true;
     }
 
-    public static int cal(String s, int pixIndex) {
-        int cnt = 1;
-        while(true){
-            char left = s.charAt(pixIndex - cnt);
-            char right = s.charAt(pixIndex + cnt);
-            if(left != right) break;
-            if(pixIndex-cnt-1 < 0 || pixIndex+cnt+1 == s.length()) break;
-            cnt++;
-        }
-        return cnt;
-    }
 }
