1df54559c8f97f442b07bf103bf531c80a88d11e
tjdwns4537 <tjdwns4537@naver.com>
2023-03-31 21:34:23 +0900

    feat: 다리를지나는트럭

---
+++ ProgrammersLEVEL2/src/다리를지나는트럭.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class 다리를지나는트럭 {
+    /**
+     * TODO
+     *  - 트럭 여러대가 일차선 도로를 건너는데 걸리는 최소한의 시간
+     *  -
+     * **/
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < truck_weights.length; i++) {
+            int sum = 0;
+            sum += truck_weights[i];
+
+        }
+
+        return answer;
+    }
+}
