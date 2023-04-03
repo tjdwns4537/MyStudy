bbf486e72c81370e8cd7cf805b983588298297a9
tjdwns4537 <tjdwns4537@naver.com>
2023-04-03 17:12:08 +0900

    fix: 다리를 지나는 트럭

--- ProgrammersLEVEL2/src/다리를지나는트럭.java
+++ ProgrammersLEVEL2/src/다리를지나는트럭.java
@@ -1,27 +1,48 @@
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class 다리를지나는트럭 {
     /**
      * TODO
      *  - 트럭 여러대가 일차선 도로를 건너는데 걸리는 최소한의 시간
-     *  -
+     *  - 큐를 활용
+     *  - 큐에 들어올 때만 시간을 +1
+     *  - 나갈 때는 시간이 올라가지 않음
+     *  1) 트럭이 들어오기 전에 bridge_length 조건 확인
+     *  2) 큐에 있는 트럭의 무게가 weight를 넘지 않는지 확인
      * **/
 
     public static void main(String[] args) {
-
+        int len = 100;
+        int weight = 100;
+        int[] arr = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+        int solution = solution(len, weight, arr);
+        System.out.println(solution);
     }
 
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
+        int answer;
+        int sum = 0;
+        int time = 0;
 
-        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new LinkedList<>();
 
         for (int i = 0; i < truck_weights.length; i++) {
-            int sum = 0;
-            sum += truck_weights[i];
-
+            int truckWeight = truck_weights[i];
+            while(true){
+                if(bridge_length <= queue.size()) sum -= queue.poll();
+                if (weight >= sum + truckWeight) {
+                    queue.add(truckWeight);
+                    sum += truckWeight;
+                    time++;
+                    break;
+                }
+                queue.add(0);
+                time++;
+            }
         }
 
+        answer = bridge_length + time;
         return answer;
     }
 }
